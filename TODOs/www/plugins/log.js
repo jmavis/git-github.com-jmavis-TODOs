c08301440d575425db3a63b234aeeb8d8caffcfb
jmavis <jaredmavis@outlook.com>
2014-12-04 22:41:56 -0800

    Basic logger

--- TODOs/www/plugins/log.js
+++ TODOs/www/plugins/log.js
@@ -1,3 +1,33 @@
 (function() {
-	
+	var logLevels = {
+		FULL: "FULL",
+		ERROR: "ERROR",
+		NONE: "NONE",
+	}
+
+	var recordFunctions = {};
+
+	recordFunctions[logLevels.FULL] = function(log) {
+		console.log(log);
+	}
+
+	recordFunctions[logLevels.ERROR] = function(log) {
+		console.log(log);
+	}
+
+	recordFunctions[logLevels.NONE] = function() {}
+
+
+	var currentLogs = {
+		"app": logLevels.FULL,
+		"TodosController": logLevels.FULL,
+	}
+
+	window.loggerInstance = function(name) {
+		var recordFunction = recordFunctions[currentLogs[name]] || function(){};
+
+		return {
+			record: recordFunction,
+		};
+	}
 })()
