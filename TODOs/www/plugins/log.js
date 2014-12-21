915cb9f4a9b45cc2920be7da4e032ac71ce9e3f3
jmavis <jaredmavis@outlook.com>
2014-12-20 23:58:45 -0800

    Color logs

--- TODOs/www/plugins/log.js
+++ TODOs/www/plugins/log.js
@@ -14,18 +14,18 @@
 	window.loggerInstance = function(name) {
 		var logLevel = _.isNumber(currentLogs[name]) ? currentLogs[name] : logTypes.DEBUG;
 		var type = logLevel
-		var recordLog = function(log) {
-			console.log(name + ": " + log);
+		var recordLog = function(log, color) {
+			console.log("%c" + name + ": " + log, "color:" + color);
 		}
 		return {
 			d: logLevel >= logTypes.DEBUG ? function(message) {
-				recordLog("DEBUG: " + message);
+				recordLog("DEBUG: " + message, "black");
 			} : function(){},
 			w: logLevel >= logTypes.WARNING ? function(message) {
-				recordLog("WARNING: " + message);
+				recordLog("WARNING: " + message, "yellow");
 			} : function(){},
 			e: logLevel >= logTypes.ERROR ? function(message) {
-				recordLog("ERROR: " + message);
+				recordLog("ERROR: " + message, "red");
 			} : function(){},
 		};
 	}
